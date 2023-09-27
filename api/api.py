3746652a8421e6745a9d8af48fcd5649b54e669d
M. Thomas <m.thomas@gso.schule.koeln>
2023-09-27 15:01:49 +0200

    return index, json in python-datei

--- api/api.py
+++ api/api.py
@@ -1,12 +1,34 @@
 import flask
 from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
 from flask import jsonify   # übersetzt python-dicts in json
+from flask import send_file 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
+
+
+menu =[{    "name":"Pizza Mario",
+            "belag": [
+                {1:"Mozarella"},
+                {2:"Tomatensauce"}
+            ],
+            "preis": 9.50
+        },
+        {
+            "name":"Pizza Luigi",
+            "belag": [
+                {1:"Mozarella"},
+                {2:"Spinat"}
+            ],
+            "preis": 12.50
+        }
+]
+
+
+
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Tischreservierung</h1>"
+    return send_file('index.html')
 
 app.run()
