f4867151b097f78b18d3cbe378cad4070e062a72
M.Thomas <m.thomas@gso.schule.koeln>
2024-11-13 21:03:42 +0100

    Aufräumen

--- api/api.py
+++ api/api.py
@@ -1,22 +1,10 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
 
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=200)
 def home():
     return FileResponse('index.html')
-
-
-@app.get('/freieTische', status_code=200)
-def freie_tische(zeitpunkt: str, response: Response):
-    # TODO wunschzeitpunkt auf nächste halbe Stunde aufrunden (18:00 --> 18:30)
-    # TODO SQLite Datenbank erzeugen (create_buchungssystem.sql) und einbinden
-    # TODO Abfrage an SQLite Datenbank absetzen
-    # TODO Ergebnis der Abfrage als JSON zurückgeben
-    # TODO Überprüfen, ob Parameter dem Muster yyyy-mm-dd hh:mm entspricht
-    
-    return zeitpunkt
-    
--- api/readme.md
+++
@@ -1,6 +0,0 @@
-# /api
-Übersicht der Dateien:
-
-- **`api.py`**: Hier lebt unsere FastAPI
-- **`requirements.txt`**: Notwendige Pakete, die zur Arbeit installiert werden müssen. Z.B. mit `pip install -r requirements.txt`
-- **`create_buchungssystem.sql`**: Sript zum erstellen einer SQLite Datenbank.
