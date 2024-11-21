f71fa5b64c54e5841126cb98817cea76c01ac52e
M.Thomas <m.thomas@gso.schule.koeln>
2024-11-21 12:40:25 +0100

    Post Endpoint Skelleton

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
 
@@ -8,3 +8,13 @@ app = FastAPI()
 @app.get("/", status_code=200)
 def home():
     return FileResponse('index.html')
+
+@app.get("/menu", status_code=200)
+def get_menu():
+    return FileResponse('testdata/get_menue.json')
+
+@app.post("/order/", status_code=201)
+def post_order_request():
+    # TODO JSON mit Pydantic vallidieren
+    # TODO Beispiel-Response zurückgeben
+    return None
